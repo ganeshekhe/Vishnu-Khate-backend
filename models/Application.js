d22c98c139c7d8f9a36deffab8a9f67b46aa8ba0
ganeshekhe <ganeshekhe2002@gmail.com>
2025-09-23 17:58:19 +0530

    Updated Application model and application routes

--- models/Application.js
+++ models/Application.js
@@ -1,74 +1,5 @@
 
-// const mongoose = require("mongoose");
-// const ApplicationSchema = new mongoose.Schema(
-//   {
-//     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-//     operator: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-//     service: { type: mongoose.Schema.Types.ObjectId, ref: "Service" },
-//     subService: { _id: String, name: String },
-//     data: { type: Object, default: {} },
-//     documents: [{ filename: String, fileId: mongoose.Schema.Types.ObjectId }],
-//     userProfile: {
-//       name: String,
-//       caste: String,
-//       gender: String,
-//       dob: Date,
-//       profileDocs: [
-//         { filename: String, fileId: mongoose.Schema.Types.ObjectId, filepath: String },
-//       ],
-//     },
-
-//     // ✅ Application status
-//     status: {
-//       type: String,
-//       enum: [
-//         "Draft",
-//         "Submitted",
-//         "In Review",
-//         "Pending Confirmation",
-//         "Confirmed",
-//         "Rejected",
-//         "Completed",
-//       ],
-//       default: "Draft",
-//     },
-
-//     formPdf: {
-//       filename: String,
-//       fileId: mongoose.Schema.Types.ObjectId,
-//     },
-//     certificate: {
-//       filename: String,
-//       fileId: mongoose.Schema.Types.ObjectId,
-//     },
-
-//     operatorCredentials: {
-//       operatorId: { type: String, default: "" },
-//       operatorPassword: { type: String, default: "" },
-//     },
-
-//     rejectReason: { type: String, default: "" },
-//     correctionComment: { type: String, default: "" },
-
-//     // ✅ Razorpay Payment Info
-//     paymentInfo: {
-//       orderId: { type: String, default: "" },
-//       paymentId: { type: String, default: "" },
-//       signature: { type: String, default: "" },
-//       amount: { type: Number, default: 0 },
-//       paymentStatus: {
-//         type: String,
-//         enum: ["Pending", "Paid", "Failed"],
-//         default: "Pending",
-//       },
-//     },
-//   },
-//   { timestamps: true }
-// );
-
-// module.exports = mongoose.model("Application", ApplicationSchema);
 const mongoose = require("mongoose");
-
 const ApplicationSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
@@ -97,7 +28,6 @@ const ApplicationSchema = new mongoose.Schema(
         "Pending Confirmation",
         "Confirmed",
         "Rejected",
-        "Waiting",
         "Completed",
       ],
       default: "Draft",
